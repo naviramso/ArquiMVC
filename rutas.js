682c06a1858a96ed0446a4e913167a1a5cbe47b5
Rodolfo Lopez Lozano <201606416@gmail.com>
2023-06-25 22:38:51 -0400

    login y estilos cambiados

--- rutas.js
+++ rutas.js
@@ -5,16 +5,24 @@ const evento = require("./model/evento")
 
 //routes
 router.get("/", (req, res) => {
-  res.send("index");
+  res.render('login');
 });
+
+router.get("/index", (req, res) => {
+  res.render('index',{title:'mi pagina con ejs'})
+});
+router.get('/views/evento.ejs',(req,res)=>{
+  res.render('evento',{title:'Crear Evento'})
+})
+
 
 // router.get("/evento",(req, res) => {
 //     res.send("");
 // })
-router.post("/datosEvento",(req,res)=>{
-  const formData = req.body;
-  // res.json(formData);
-  evento(formData.nombreEvento, formData.descripcion, formData.fechaInicio, '8:00', formData.fechaFin, formData.ubicacion)
-})
+// router.post("/datosEvento",(req,res)=>{
+//   const formData = req.body;
+//   // res.json(formData);
+//   evento(formData.nombreEvento, formData.descripcion, formData.fechaInicio, '8:00', formData.fechaFin, formData.ubicacion)
+// })
 
 module.exports = router;
